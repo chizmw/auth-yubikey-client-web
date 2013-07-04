f68cde01a1293f84ebadccd770d960d1fd7f095a
Chisel <chisel@chizography.net>
2013-07-04 22:42:45 +0100

    Post-minting Initial Commit

---
+++ lib/Auth/YubiKey/Client/Web.pm
@@ -0,0 +1,8 @@
+package Auth::YubiKey::Client::Web;
+use strict;
+use warnings;
+
+1;
+# ABSTRACT: Auth::YubiKey::Client::Web needs a more meaningful abstract
+__END__
+# vim: ts=8 sts=4 et sw=4 sr sta
